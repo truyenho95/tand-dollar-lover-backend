467a3a6204ab497c24ba9ec4b77a284ed25a1b3c
hanktran <trannhatanhttqtd@gmail.com>
2019-03-01 15:29:13 +0700

    initial commit

---
+++ dollar-lover-spring-server/src/main/java/com/tand/dollarlover/DollarLoverApplication.java
@@ -0,0 +1,12 @@
+package com.tand.dollarlover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarLoverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DollarLoverApplication.class, args);
+    }
+
+}
